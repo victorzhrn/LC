94f1ec20f72343a5038df10c6112b2c922dffd87
Ruinan Zhang <zhangr4@rose-hulman.edu>
2017-03-03 15:47:36 +0800

    finish check black surrounded

--- checkBlackSurrounded/src/checkBlackSurrounded/Solution.java
+++ checkBlackSurrounded/src/checkBlackSurrounded/Solution.java
@@ -2,6 +2,7 @@ package checkBlackSurrounded;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Stack;
 
 /*
  * the task is to determine if the black chess on a board is surrounded by whites
@@ -15,29 +16,63 @@ public class Solution {
 		int rows = board.length;
 		int cols = board[0].length;
 		boolean[][] checkBoard = new boolean[rows][cols];
-		HashSet<int[]> allBlacks = new HashSet<int[]>();
+		Stack<int[]> allBlacks = new Stack<int[]>();
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				if (board[i][j] == 'b');
-				checkBoard[i][j] = true;
-				int[] black_loc = { i, j };
-				allBlacks.add(black_loc);
+				if (board[i][j] == 'b') {
+					checkBoard[i][j] = true;
+					int[] black_loc = { i, j };
+					allBlacks.push(black_loc);
+				}
 			}
 		}
+
+		while(!allBlacks.isEmpty()) {
+			int[] a = allBlacks.pop();
+			int x = a[0], y = a[1];
+			if (checkSurroundedByEmpty(a,rows,cols,board)){
+				checkBoard[x][y] = false;
+				board[x][y] = 'e';
+				if (x-1>=0 && board[x-1][y]=='b') {
+					int[] newBlack = {x-1,y};
+					allBlacks.push(newBlack);
+				}
+				if (x+1 < rows && board[x+1][y]=='b'){
+					int[] newBlack = {x+1,y};
+					allBlacks.push(newBlack);
+				}
+				if (y-1 >=0 && board[x][y-1]=='b'){
+					int[] newBlack = {x,y-1};
+					allBlacks.push(newBlack);
+				}
+				if (y+1 < cols && board[x][y+1]=='b'){
+					int[] newBlack = {x,y+1};
+					allBlacks.push(newBlack);
+				}
+			}
+		}
+		for (int i = 0; i < rows ; i ++){
+			for (int j = 0; j <cols; j++){
+				if (checkBoard[i][j]) return true;
+			}
+		}
+
 		return false;
 	}
 	
-	public boolean checkSurroundedByEmpty(int[] a, int rows, int cols,char[][] board){
+
+	public static boolean checkSurroundedByEmpty(int[] a, int rows, int cols, char[][] board) {
 		int x = a[0];
 		int y = a[1];
-		if (x-1>=0) {
-			boolean leftOK = (board[x][y]=='e');
-		}else{
-			boolean leftOK = false;
-		}
-		
-		
-		return false;
-		
+		boolean leftOK = false, rightOK = false, aboveOK = false, belowOK = false;
+		if (x - 1 >= 0)
+			leftOK = (board[x - 1][y] == 'e');
+		if (x + 1 < rows)
+			rightOK = (board[x + 1][y] == 'e');
+		if (y - 1 >= 0)
+			belowOK = (board[x][y - 1] == 'e');
+		if (y + 1 < cols)
+			aboveOK = (board[x][y + 1] == 'e');
+		return leftOK || rightOK || aboveOK || belowOK;
 	}
 }
--- checkBlackSurrounded/src/checkBlackSurrounded/SolutionTest.java
+++ checkBlackSurrounded/src/checkBlackSurrounded/SolutionTest.java
@@ -14,5 +14,33 @@ public class SolutionTest {
 		
 		assertTrue(Solution.checkBlack(board));
   	}
+	
+
+	@Test
+	public void test2() {
+		char[][] board = {{'e','w','w','b'},
+				{'w','b','e','w'},
+				{'e','w','w','e'}};
+		
+		assertTrue(Solution.checkBlack(board));
+  	}
+	
+	@Test
+	public void test3() {
+		char[][] board = {{'e','w','w','e'},
+				{'w','b','e','w'},
+				{'e','w','w','e'}};
+		
+		assertFalse(Solution.checkBlack(board));
+  	}
+	
+	@Test
+	public void test4() {
+		char[][] board = {{'b','b','w','e'},
+				{'w','b','e','w'},
+				{'e','w','b','e'}};
+		
+		assertFalse(Solution.checkBlack(board));
+  	}
 
 }
